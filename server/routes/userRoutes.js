83a7b2e54a146352a76568a9fed7811ea118124f
LOGANBLUE1 <mrpareekaditya@gmail.com>
2025-03-23 19:44:56 +0530

    initialized backend

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+// Import the required modules
+const express = require("express")
+const router = express.Router()
+
+const {
+  resetPasswordToken,
+  resetPassword,
+} = require("../controllers/resetPassword")
+
+const { auth } = require("../middleware/auth")
+
+
+// Export the router for use in the main application
+module.exports = router
